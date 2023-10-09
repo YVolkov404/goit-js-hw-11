041de93ad1b434a7db98210a4cd9df6479052cef
Yuriy Volkov <sir.volkov@outlook.com>
2023-10-09 20:21:16 +0300

    upd: task cmplt

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
+import axios from 'axios';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { simpleLightboxOptions, notifyOptions } from './options';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { createMarkup } from './create-markup';
-import ApiService from './class';
 import PreLoadState from './preload-state';
 
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -11,10 +11,6 @@ import PreLoadState from './preload-state';
 const form = document.querySelector('form');
 const gallery = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
-
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-const apiService = new ApiService();
 
 const lightbox = new SimpleLightbox(
   '.gallery .photo-card a',
@@ -25,6 +21,26 @@ const preLoadState = new PreLoadState({
   selector: '.load-more',
 });
 
+let searchQuery = '';
+let images;
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+const axios = require('axios').default;
+
+axios.defaults.baseURL = 'https://pixabay.com/api';
+
+const instance = axios.create({
+  baseURL: 'https://pixabay.com/api',
+  key: '39908765-01641b9876d1c1af0468ed447',
+  q: `${searchQuery}`,
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: true,
+  page: 1,
+  per_page: 40,
+});
+
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 form.addEventListener('submit', onSearchForm);
@@ -32,15 +48,38 @@ loadMoreBtn.addEventListener('click', onloadMore);
 
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+async function fetchImages(searchQuery) {
+  try {
+    return await axios.get(`/`, {
+      params: {
+        baseURL: 'https://pixabay.com/api',
+        key: '39908765-01641b9876d1c1af0468ed447',
+        q: `${searchQuery}`,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page: 1,
+        per_page: 40,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 async function onSearchForm(e) {
   e.preventDefault();
 
   clearPage();
-  apiService.query = e.currentTarget.elements.searchQuery.value.trim();
+  searchQuery = e.currentTarget.searchQuery.value.trim();
+  resetPage();
+
   form.reset();
   preLoadState.show();
 
-  if (apiService.query === '') {
+  if (searchQuery === '') {
     preLoadState.hide();
     Notify.warning(
       'Please type your query in the input field for receiving search result',
@@ -50,16 +89,18 @@ async function onSearchForm(e) {
   }
 
   try {
-    const data = await apiService.fetchImages();
-    const images = data.hits;
+    const response = await fetchImages(searchQuery);
+    const images = response.data.hits;
 
     if (images.length === 0) {
       form.reset();
     }
 
+    incrementPageAndPerPage();
+
     createMarkup(images);
     lightbox.refresh();
-  } catch {
+  } catch (error) {
     preLoadState.hide();
     Notify.warning(
       'Sorry, there are no images matching your search query. Please try again',
@@ -68,27 +109,36 @@ async function onSearchForm(e) {
   }
 }
 
-// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
 async function onloadMore(e) {
   try {
-    const data = await apiService.fetchImages();
-    const images = data.hits;
-    const totalHits = data.totalHits;
+    const response = await fetchImages(searchQuery);
+    const totalHits = response.data.totalHits;
 
-    if (totalHits < apiService.per_page) {
+    if (totalHits < instance.per_page) {
       clearPage();
     }
 
     createMarkup(images);
     lightbox.refresh();
-  } catch {
+  } catch (error) {
     preLoadState.hide();
     Notify.warning(
       "We're sorry, but you've reached the end of search results.",
       notifyOptions
     );
   }
+}
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+function incrementPageAndPerPage() {
+  instance.page += 1;
+  instance.per_page += 40;
+}
+
+function resetPage() {
+  instance.page = 1;
+  instance.per_page = 40;
 }
 
 function clearPage() {
